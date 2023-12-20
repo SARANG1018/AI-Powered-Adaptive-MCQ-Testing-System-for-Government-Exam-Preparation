bf7d6b8e05c32589067ec0c2ede1ca401d9903df
SARANG1018 <sarangkadakia10@gmail.com>
2023-12-20 12:35:15 +0530

    Segregation of all ML models into backend

--- SIH_adaptive_learning/questions/controller.py
+++ SIH_adaptive_learning/questions/controller.py
@@ -1,17 +1,16 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
-import random
 from .serializers import QuestionSerializer
 from random import sample
 import math
+from ml_models.algorithms.IRT import next_difficulty_onIRT_3PL, next_ability_onIRT_3PL
 
 
 def random_question(json):
     try:
         # Get 25 random questions
         all_questions = list(Questions.objects.all())
-        Questions.objects.get
 
         random_questions = sample(all_questions, min(25, len(all_questions)))
 
@@ -24,119 +23,109 @@ def random_question(json):
         return {"error": str(e)}
 
 
-# calculate ability_parameter - student skill
-def calculate_ability_parameter(
-    total_responses_of_question, total_question_count, num_options=4
-):
-    try:
-        return math.log(
-            sum(total_responses_of_question)
-            / (num_options * total_question_count)
-            / (
-                1
-                - sum(total_responses_of_question)
-                / (num_options * total_question_count)
-            )
-        )
-    except:
-        if sum(total_responses_of_question) == 0:
-            return -4
-        else:
-            return 4
-
-
 def update_question(
     correct_count,  # woh question ko kitno ne sahi kiya hai
     time_required,  # time for that question by current user
     avg_time_for_question,  # avg of all previous attemps
     correct,  # is ans correct or not
     response_count,  # total Question attempts for that question
+    user_proficiency,
     difficulty=0.3,
 ):
     accuracy = correct_count / response_count
-
-    if correct:
-        # Increase difficulty when the answer is correct
-        difficulty += 0.1 * (time_required / avg_time_for_question) * accuracy
-    else:
-        # Decrease difficulty when the answer is incorrect
-        difficulty -= 0.1 * (time_required / avg_time_for_question) * accuracy
-
+    difficulty = next_difficulty_onIRT_3PL(user_proficiency, difficulty)
     return difficulty
 
-import random
+
+# calculate ability_parameter - student skill
+# def calculate_ability_parameter(
+#     total_responses_of_question, total_question_count, num_options=4
+# ):
+#     try:
+#         return math.log(
+#             sum(total_responses_of_question)
+#             / (num_options * total_question_count)
+#             / (
+#                 1
+#                 - sum(total_responses_of_question)
+#                 / (num_options * total_question_count)
+#             )
+#         )
+#     except:
+#         if sum(total_responses_of_question) == 0:
+#             return -4
+#         else:
+#             return 4
 
 
-# calculate lesson_difficulty
-def calculate_lesson_difficulty(
-    correct_responses,  # total User ka us set mein  kitna correct hai
-    total_questions_in_lesson_set: int
-    # Total Question in previous set
+def update_user_proficiency(user_proficiency, ability_of_student):
+    ability_of_student = next_ability_onIRT_3PL(user_proficiency, ability_of_student)
+
+
+
+def generate_questions(
+    num_questions, questions, user_proficiency, assigned_questions, kmeans_clusters
 ):
-    try:
-        return math.log(
-            (1 - (sum(correct_responses) / total_questions_in_lesson_set))
-            / (sum(correct_responses) / total_questions_in_lesson_set)
-        )
-    except:
-        if all(response == 0 for response in correct_responses):
-            return 4
-        else:
-            return -4
+    distances = {}
+
+    # Calculate distances between user proficiency and question difficulties
+    for question_id, difficulty in questions.items():
+        distance = abs(difficulty - user_proficiency)
+        distances[question_id] = distance
+
+    # Sort questions based on distances
+    sorted_questions = sorted(distances.keys(), key=lambda x: distances[x])
+
+    # Filter out questions that have already been assigned
+    available_questions = [q for q in sorted_questions if q not in assigned_questions]
+
+    # Select new questions for the user
+    selected_questions = available_questions[:num_questions]
+
+    # Update the list of assigned questions
+    assigned_questions.extend(selected_questions)
+
+    return selected_questions
 
 
-# calculating correctness_prob
-def calculate_correctness_prob(ability_parameter, lesson_difficulty):
-    return ((math.e) ** (ability_parameter - lesson_difficulty)) / (
-        1 + (math.e) ** (ability_parameter - lesson_difficulty)
-    )
+# def generate_questions(
+#     num_questions,  # Total Questions to be generated in next set
+#     questions: dict,  # Difficulties of all Questions on specialization
+#     user_proficiency,
+# ):
+
+#     distances =dict()
+
+#     for id,difficulty in questions.values():
+#         distance= difficulty-user_proficiency
+#         distances.append(id, distance)
+
+#     questions_set=sorted(distances.keys(),key=lambda x: abs(distances.get(x)))
+#     return questions_set[:25]
 
 
-def update_user_proficiency(
-    user_proficiency,
-    lesson_difficulty,  # To be calculated
-    correctness_probability,
-    total_responses_of_question,# Us question ke kitne Response hai
-    total_question_count, # Total Questions in a domain
-):
-    ability_parameter = calculate_ability_parameter(
-        total_responses_of_question, 4, total_question_count
-    )
-    new_user_proficiency = (
-        user_proficiency
-        + (lesson_difficulty - ability_parameter) * correctness_probability
-    )
+# import random
 
-    # range (0-1)
-    new_user_proficiency = max(0, min(1, new_user_proficiency))
+# # calculate lesson_difficulty
+# def calculate_lesson_difficulty(
+#     correct_responses,  # total User ka us set mein  kitna correct hai
+#     total_questions_in_lesson_set: int
+#     # Total Question in previous set
+# ):
+#     try:
+#         return math.log(
+#             (1 - (sum(correct_responses) / total_questions_in_lesson_set))
+#             / (sum(correct_responses) / total_questions_in_lesson_set)
+#         )
+#     except:
+#         if all(response == 0 for response in correct_responses):
+#             return 4
+#         else:
+#             return -4
 
-    return new_user_proficiency
 
-
-def generate_questions(num_questions# Total Questions to be generated in next set
-                       , question_difficulties# Difficulties of all Questions on specialization
-                       , user_proficiency): 
-    questions = []
-
-    # Define a range around user proficiency for selecting questions
-    range_threshold = 0.3  # for now
-
-    for _ in range(num_questions):
-        # Generating a random value within the range around user proficiency
-        rand_value = random.uniform(
-            user_proficiency - range_threshold, user_proficiency + range_threshold
-        )
-
-        # Finding the closest question difficulty to the random value within the range
-        closest_difficulty = min(
-            question_difficulties, key=lambda x: abs(x - rand_value)
-        )
-
-        # Determine if the user can answer the question based on the difference between user proficiency and question difficulty
-        can_answer = user_proficiency >= closest_difficulty
-
-        # Creating a tuple with question difficulty and a flag indicating if the user can answer it
-        question = (closest_difficulty, can_answer)
-        questions.append(question)
-
-    return questions
+# # calculating correctness_prob
+# def calculate_correctness_prob(ability_parameter, lesson_difficulty):
+#     return ((math.e) ** (ability_parameter - lesson_difficulty)) / (
+#         1 + (math.e) ** (ability_parameter - lesson_difficulty)
+#     )
